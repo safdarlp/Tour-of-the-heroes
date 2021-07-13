7e7ada3537c0a625ac16d4f5ad0fa2bd240293f8
SAFDAR KHAN <SAFDARLP@GMAIL.COM>
2021-07-12 23:05:42 -0500

    Update heroes

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -62,12 +62,20 @@ export class HeroService {
     }
 
     /** PUT: update the hero on the server */
-updateHero(hero: Hero): Observable<any> {
-  return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
-    tap(_ => this.log(`updated hero id=${hero.id}`)),
-    catchError(this.handleError<any>('updateHero'))
-  );
-}
+    updateHero(hero: Hero): Observable<any> {
+      return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+        tap(_ => this.log(`updated hero id=${hero.id}`)),
+        catchError(this.handleError<any>('updateHero'))
+      );
+    }
+
+    /** POST: add a new hero to the server */
+      addHero(hero: Hero): Observable<Hero> {
+      return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+        tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+        catchError(this.handleError<Hero>('addHero'))
+      );
+    }
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
   this.messageService.add(`HeroService: ${message}`);
